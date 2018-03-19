53a27d2d55dec51e6154a5c801894f31022e2c5c
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-03-19 23:31:24 +0100

    Fix crash when libfuse is not available but fusepy available

--- parsec/cli.py
+++ parsec/cli.py
@@ -5,10 +5,10 @@ from parsec.backend.cli import backend_cmd
 from parsec.ui import shell
 try:
     from parsec.ui.fuse import cli as fuse_cli
-except ImportError:
+except (ImportError, EnvironmentError) as exc:
     @click.command()
     def fuse_cli():
-        raise RuntimeError('No available, is fusepy installed ?')
+        raise RuntimeError('No available (%s), is fusepy&libfuse installed ?' % exc)
 
 
 @click.group()

7715c5925e6806048b26952cc172eebfdda9098f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-07-03 10:53:30 +0200

    Hold my beer...

--- parsec/core/backend_events_manager.py
+++ parsec/core/backend_events_manager.py
@@ -46,45 +46,93 @@ class BackendEventsManager(BaseAsyncComponent):
         self.backend_addr = backend_addr
         self._nursery = None
         self._subscribed_events = set()
-        self._event_listener_task_info = None
+        self._need_task_restart = trio.Event()
+        self._el_task_info = None
+        self._rm_task_info = None
+        get_signal('backend.event.subscribe').connect(self._on_event_subscribe, weak=True)
+        get_signal('backend.event.unsubscribe').connect(self._on_event_unsubscribe, weak=True)
+
+    async def _on_event_subscribe(self, sender, event, subject=None):
+        assert sender == 'local'
+        self._subscribed_events.add((event, subject))
+        self._need_task_restart.set()
+
+    async def _on_event_unsubscribe(self, sender, event, subject=None):
+        assert sender == 'local'
+        try:
+            self._subscribed_events.remove((event, subject))
+        except KeyError:
+            return
+        self._need_task_restart.set()
 
     async def _init(self, nursery):
         self._nursery = nursery
-        self._event_listener_task_info = await nursery.start(self._event_listener_task)
+        await self._restart_el_task()
+        self._rm_task_info = await self._nursery.start(self._restart_manager_task)
 
     async def _teardown(self):
-        cancel_scope, closed_event = self._event_listener_task_info
+        await self._close_task()
+        await self._close_rm_task()
+
+    async def _restart_el_task(self):
+        new_elt_info = await self._nursery.start(self._event_listener_task)
+        if self._el_task_info:
+            await self._close_task()
+        self._el_task_info = new_elt_info
+
+    async def _close_el_task(self):
+        cancel_scope, closed_event = self._el_task_info
         cancel_scope.cancel()
         await closed_event.wait()
+        self._el_task_info = None
 
-    async def subscribe_backend_event(self, event, subject=None):
-        """
-        Raises:
-            KeyError: if event/subject couple has already been previously subscribed.
-        """
-        async with self._lock:
-            key = (event, subject)
-            if key in self._subscribed_events:
-                raise KeyError("%s@%s already subscribed" % key)
+    async def _close_rm_task(self):
+        cancel_scope, closed_event = self._rm_task_info
+        cancel_scope.cancel()
+        await closed_event.wait()
+        self._rm_task_info = None
 
-            self._subscribed_events.add(key)
-            logger.debug("Subscribe %s@%s, restarting event listener" % (event, subject))
-            await self._teardown()
-            await self._init(self._nursery)
+    # async def subscribe_backend_event(self, event, subject=None):
+    #     """
+    #     Raises:
+    #         KeyError: if event/subject couple has already been previously subscribed.
+    #     """
+    #     async with self._lock:
+    #         key = (event, subject)
+    #         if key in self._subscribed_events:
+    #             raise KeyError("%s@%s already subscribed" % key)
 
-    async def unsubscribe_backend_event(self, event, subject=None):
-        """
-        Raises:
-            KeyError: if event/subject couple has not been previously subscribed.
-        """
-        async with self._lock:
-            self._subscribed_events.remove((event, subject))
-            logger.debug("Unsubscribe %s@%s, restarting event listener" % (event, subject))
-            await self._teardown()
-            await self._init(self._nursery)
+    #         self._subscribed_events.add(key)
+    #         logger.debug("Subscribe %s@%s, restarting event listener" % (event, subject))
+    #         await self._teardown()
+    #         await self._init(self._nursery)
+
+    # async def unsubscribe_backend_event(self, event, subject=None):
+    #     """
+    #     Raises:
+    #         KeyError: if event/subject couple has not been previously subscribed.
+    #     """
+    #     async with self._lock:
+    #         self._subscribed_events.remove((event, subject))
+    #         logger.debug("Unsubscribe %s@%s, restarting event listener" % (event, subject))
+    #         await self._teardown()
+    #         await self._init(self._nursery)
+
+    async def _restart_manager_task(self, *, task_status=trio.TASK_STATUS_IGNORED):
+        closed_event = trio.Event()
+        try:
+            with trio.open_cancel_scope() as cancel_scope:
+                task_status.started((cancel_scope, closed_event))
+
+                while True:
+                    await self._need_task_restart.wait()
+                    self._need_task_restart.clear()
+
+        finally:
+            closed_event.set()
 
     async def _event_listener_task(self, *, task_status=trio.TASK_STATUS_IGNORED):
-        # Copy `self._subscribed_event` to avoid concurrent modifications
+        # Copy `self._subscribed_events` to avoid concurrent modifications
         subscribed_events = self._subscribed_events.copy()
 
         async def _event_pump(sock):
@@ -92,13 +140,10 @@ class BackendEventsManager(BaseAsyncComponent):
             for event, subject in subscribed_events:
                 await sock.send({"cmd": "event_subscribe", "event": event, "subject": subject})
                 rep = await sock.recv()
-                if rep["status"] != "ok":
+                if rep.get("status") != "ok":
                     raise SubscribeBackendEventError(
                         "Cannot subscribe to event `%s@%s`: %r" % (event, subject, rep)
                     )
-
-            # Useful for tests to know when we actually start to listen for backend events
-            get_signal("backend_event_manager_listener_started").send()
 
             while True:
                 await sock.send({"cmd": "event_listen"})
@@ -124,6 +169,7 @@ class BackendEventsManager(BaseAsyncComponent):
                 while True:
                     try:
                         sock = await backend_connection_factory(self.backend_addr, self.device)
+                        get_signal('backend.online').send('local')
                         await _event_pump(sock)
                     except (
                         BackendNotAvailable,
@@ -131,6 +177,7 @@ class BackendEventsManager(BaseAsyncComponent):
                         trio.ClosedStreamError,
                     ) as exc:
                         # In case of connection failure, wait a bit and restart
+                        get_signal('backend.offline').send('local')
                         logger.debug(
                             "Connection lost with backend ({}), restarting connection...", exc
                         )

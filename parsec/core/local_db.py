6aba9ae6aeddffa6c6fdfb443572ef4880844c70
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-07-06 20:19:45 +0200

    Implement local_db, test_devices_manager.py is passing

--- parsec/core/local_db.py
+++ parsec/core/local_db.py
@@ -18,11 +18,15 @@ class LocalDBMissingEntry(LocalDBError):
 
 class LocalDB:
     def __init__(self, path):
-        self.path = Path(path)
-        self.path.mkdir(parents=True, exist_ok=True)
+        self._path = Path(path)
+        self._path.mkdir(parents=True, exist_ok=True)
+
+    @property
+    def path(self):
+        return str(self._path)
 
     def get(self, access):
-        file = self.path / access["id"]
+        file = self._path / access["id"]
         try:
             raw = file.read_bytes()
         except FileNotFoundError:
@@ -31,11 +35,11 @@ class LocalDB:
 
     def set(self, access, manifest):
         ciphered = self._encrypt_for_local(manifest)
-        file = self.path / access["id"]
+        file = self._path / access["id"]
         file.write_bytes(ciphered)
 
     def clear(self, access):
-        file = self.path / access["id"]
+        file = self._path / access["id"]
         try:
             file.unlink()
         except FileNotFoundError:
